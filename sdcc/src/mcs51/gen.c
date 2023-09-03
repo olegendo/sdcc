b5517143686d589476de2ac7e2a59eb3810b44eb
Oleg Endo <olegendo@gcc.gnu.org>
2023-09-03 16:41:52 +0900

    add __sdcc_banked_jmp for improved banked no-return call and tailcall

--- sdcc/src/mcs51/gen.c
+++ sdcc/src/mcs51/gen.c
@@ -3704,7 +3704,10 @@ genCall (iCode * ic)
           ic->rCallUsed = bitVectSetBit (ic->rCallUsed, R1_IDX);
           ic->rCallUsed = bitVectSetBit (ic->rCallUsed, R2_IDX);
 
-          emitcode (callinsn, "__sdcc_banked_call");
+          if (ic->tailcall)
+            emitcode (callinsn, "__sdcc_banked_jmp");
+          else
+            emitcode (callinsn, "__sdcc_banked_call");
         }
     }
   else
